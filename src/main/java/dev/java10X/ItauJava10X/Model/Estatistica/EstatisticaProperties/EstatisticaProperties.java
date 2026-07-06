e1b70583c87fbce73f23e768a25fedcae96a2df2
victorlunes <victorllunes@gmail.com>
2026-07-06 19:32:56 -0300

    refactor: ajustando validacoes segundos

--- src/main/java/dev/java10X/ItauJava10X/Model/Estatistica/EstatisticaProperties/EstatisticaProperties.java
+++ src/main/java/dev/java10X/ItauJava10X/Model/Estatistica/EstatisticaProperties/EstatisticaProperties.java
@@ -1,16 +1,13 @@
 package dev.java10X.ItauJava10X.Model.Estatistica.EstatisticaProperties;
 
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "estatistica")
 public record EstatisticaProperties(
         @NotNull
+        @Positive
         Integer segundos
 ) {
-        public EstatisticaProperties {
-                if (segundos < 60) {
-                        throw new IllegalArgumentException("Segundos deve ser maior que 60");
-                }
-        }
 }
